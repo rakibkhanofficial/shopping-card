bf88f34183cb88900621315b70acd7086ef06854
rakibkhanofficial <khanrakib218@gmail.com>
2023-01-25 11:31:43 +0600

    added strip payment method

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_SECRET_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
