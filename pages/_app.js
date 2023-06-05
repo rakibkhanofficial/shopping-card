81255d8dcedf2870e971ba3b2bdb0370b110794c
rakibkhanofficial <khanrakib218@gmail.com>
2023-06-05 11:55:57 +0600

    fix: changes position

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
 import { RecoilRoot } from "recoil"
 import { Toaster } from 'react-hot-toast'
+import MessengerChat from '../components/MessengerChat';
+
 
 
 function MyApp({ Component, pageProps }) {
   return <RecoilRoot>
     <Toaster position='bottom-center' />
     <Component {...pageProps} />
+    <MessengerChat/>
   </RecoilRoot>
+  
+
 }
 
 export default MyApp
